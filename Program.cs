1e27c2981361173b6c455cee0174e22409b67bc2
Lucas Caetano <lucas.caetano@dtidigital.com.br>
2024-08-28 18:40:51 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using Estrutura_Base.Services;
+
+namespace Estrutura_Base
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            // Configure the FibonacciService as a scoped service to be injected by the dependency injection
+            builder.Services.AddScoped<FibonacciService>();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
